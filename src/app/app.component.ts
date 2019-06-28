ad80cd456e0ca69ef270e29cf1912f9af712e5ba
delAmux <luisdelamo@gmail.com>
2019-06-29 00:48:47 +0100

    ref:#master pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
+  name = 'delAmux';
+  arrayNumbers = [0, 1, 2, 3, 4, 5];
+  PI = Math.PI;
+  aNumber = 0.234456;
+  salary = 40450.65;
+  hero = {
+    name: 'Peter Parker',
+    alias: 'Spiderman',
+    age: 16,
+    address: {
+      street: 'my street',
+      number: 20
+    }
+  };
+
+  promiseValue = new Promise( (resolve, reject) => {
+    setTimeout(() => resolve('The data arrived'), 3500);
+  });
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {registerLocaleData} from '@angular/common';
+import localeES from '@angular/common/locales/es';
+import localeExtra from '@angular/common/locales/extra/es';
+registerLocaleData(localeES, 'es', localeExtra);
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
